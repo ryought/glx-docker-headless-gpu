533adf138e2315214172c46e0fa7fc9a6e8e95f7
Ryo Nakabayashi <ryonakabayashi@gmail.com>
2019-12-04 18:09:02 +0900

    add utils

---
+++ utils/install-docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# install docker-ce and nvidia-container-runtime on Debian/Ubuntu
+# this requires sudo
+
+# https://nvidia.github.io/nvidia-container-runtime/
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
+  sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+sudo apt-get update
+sudo apt-get install nvidia-container-runtime
+
+# https://docs.docker.com/v17.09/engine/installation/linux/docker-ce/ubuntu/#upgrade-docker-ce
+curl -fsSL get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+# to test (with gpu)
+# docker run --gpus all nvidia/cuda nvidia-smi
+# or (without gpu)
+# docker run hello-world
---
+++ utils/install-nvidia-driver.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://us.download.nvidia.com/tesla/418.87/NVIDIA-Linux-x86_64-418.87.01.run
+sudo bash NVIDIA-Linux-x86_64-418.87.01.run
